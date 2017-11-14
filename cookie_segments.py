6fc17f39c80f16f87dced12bd7d445d5df18530e
Daniel Dehaas <Dantd7@gmail.com>
2017-11-13 22:56:08 -0800

    Update cookie_segments.py

--- cookie_segments.py
+++ cookie_segments.py
@@ -1,48 +1,121 @@
 #!/usr/bin/python3
 
+#!/usr/bin/python3
+
 import re
 import sys
 
 # TODO: make decision re: cmd line/validation
 
-# eval = open("evaluator.log","r")
-#baseline = open("baseline.log","r")
+# cookie_jar = list()
+segment_set = list()
 
-# new_dict = dict()
-cookie_jar = set()
-# set_of_segs = set()
-segment_set = set()
+cookie_re = re.compile('([0-9a-f]{32})')
+segment_re = re.compile('[A-Z][0-9]{5}_[0-9]{5}')
 
-cookie = re.compile('([0-9a-f]{32})')
-segment = re.compile('[A-Z][0-9]{5}_[0-9]{5}')
+# dict with cookies as keys
+eval_cookie_map = dict()
+eval_segment_map = dict()
 
-with open("evaluator.log") as f:
+with open("eval.log") as f:
     for line in f:
-        # cookie_jar = cookie.findall(line)
-        cookie_jar.update(cookie.findall(line))
-        segment_set = segment.findall(line)
-        # set_of_segs.update(segment.findall(line))
-        print(segment_set)
-        print('doodoo')
-        
-        # for word in cookie_jar:
-        #     print(word)
+        cookie_jar = cookie_re.search(line)
+        segment_set = segment_re.findall(line)
+        if cookie_jar != None:
+            eval_cookie_map.update({cookie_jar.groups(): segment_set})
+            for seg in segment_set:
+                if seg in eval_segment_map:
+                    eval_segment_map[seg].append(cookie_jar.groups())
+                else:
+                    eval_segment_map.update({seg: list(cookie_jar.groups())})
 
-# # f.close()
-print(len(segment_set))
-print(len(cookie_jar))
-# with open("evaluator.log") as f:
-#     for line in f:
-#         segment_set = segment.findall(line)
+# for k in eval_segment_map:
+#     print(k)
+#     for v in eval_segment_map[k]:
+#         print(v)
+# print(len(eval_cookie_map))
 
-# for word in cookie_jar:
-#     print(word)
+# dict with cookies as keys
+base_cookie_map = dict()
+base_segment_map = dict()
 
-# for seg in set_of_segs:
-#     print(seg)
-# print(sorted(set_of_segs))
-# #re.findall(cookie, eval.read())
 
-# # for line in open("evaluator.log"):
-# #     re.findall(cookie, line)
-# f.close()
+with open("base.log") as f:
+    for line in f:
+        cookie_jar = cookie_re.search(line)
+        segment_set = segment_re.findall(line)
+        if cookie_jar != None:
+            base_cookie_map.update({cookie_jar.groups(): segment_set})
+            for seg in segment_set:
+                if seg in base_segment_map:
+                    base_segment_map[seg].append(cookie_jar.groups())
+                else:
+                    base_segment_map.update({seg: list(cookie_jar.groups())})
+
+# dict with segments as keys
+# eval_segment_map = dict()
+
+
+# for zed in dum_list:
+#     print(zed)
+# invert dict
+# temp = set(a for b in eval_cookie_map.values() for a in b)
+# eval_segment_map = dict((new_key, [key for key, 
+# value in eval_cookie_map.items() if new_key in temp]) for new_key in temp)
+# print(eval_segment_map)
+
+# for k in eval_cookie_map:
+#     if eval_cookie_map.get(k) == None:
+#         print("doodoo")
+#     else:
+#         print(eval_cookie_map.get(k))
+
+# for k in eval_cookie_map:
+#     for c in eval_cookie_map.get(k):
+#         eval_segment_map.update({c, k})
+
+# for k, v in eval_cookie_map.items():
+#     for x in v.items():
+#         eval_segment_map.update({x, k})
+
+
+# for key in eval_cookie_map:
+#     for value in eval_cookie_map[key]:
+#         eval_segment_map[value] = key
+
+# dict with segments as keys
+# base_segment_map = dict()
+
+# invert dict
+# for key in base_cookie_map:
+#     for value in base_cookie_map[key]:
+#         base_segment_map[value] = key
+
+
+# print(len(eval_cookie_map))
+# print(len(eval_segment_map))
+# print(len(base_cookie_map))
+# print(len(base_segment_map))
+
+# print(eval_cookie_map)
+# print(eval_segment_map)
+
+# for k in base_segment_map:
+#     print(k + ':')
+#     for v in enumerate(base_segment_map[k]):
+#         print(v)
+
+
+# print(base_segment_map)
+# reusable dict to repopulate for each report
+# results_map = dict()
+# difference_map = dict()
+# tempset = list()
+
+# for segs in base_segment_map:
+#     for cooks in base_segment_map.get(segs):    
+#            print(cooks) 
+
+
+# print(sys.getsizeof(eval_cookie_map))
+# print(sys.getsizeof(base_segment_map))
