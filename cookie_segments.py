bb2a4ddf3e4d1cb17e90197bada74c7a698756f8
Daniel Dehaas <Dantd7@gmail.com>
2017-11-14 22:39:52 -0800

    Update cookie_segments.py

--- cookie_segments.py
+++ cookie_segments.py
@@ -1,186 +1,96 @@
 #!/usr/bin/python3
 
+import collections
 import re
 import sys
 import time
 
-# TODO: make decision re: cmd line/validation
-
 start = time.time()
 
-cookie_jar = list()
-segment_set = list()
+base_cookie_map = collections.defaultdict(list)
+base_segment_map = collections.defaultdict(list)
 
-# 
-cookie_re = re.compile('([0-9a-f]{32})')
-segment_re = re.compile('[A-Z][0-9]{5}_[0-9]{5}')
-
-# dict with cookies as keys
-eval_cookie_map = dict()
-eval_segment_map = dict()
-
-# build two dicts from evaluation
-with open("evaluator-integration.log") as f:
+with open("evaluator-integration-baseline.log", "r") as f:
     for line in f:
-        cookie = cookie_re.search(line)
-        segment_set = segment_re.findall(line)
-        if cookie != None:
-            cookie = cookie.group()
-            eval_cookie_map.update({cookie: segment_set})
-            for seg in segment_set:
-                if seg in eval_segment_map:
-                    eval_segment_map[seg].append(cookie)
-                else:
-                    cookie_jar.append(cookie)
-                    eval_segment_map.update({seg: list(cookie_jar)})
-                    cookie_jar.clear()
-
-base_cookie_map = dict()
-base_segment_map = dict()
-
-# build two dicts from baseline
-with open("evaluator-integration-baseline.log") as f:
-    for line in f:
-        cookie = cookie_re.search(line)
-        segment_set = segment_re.findall(line)
-        if cookie != None:
-            cookie = cookie.group()
-            base_cookie_map.update({cookie: segment_set})
-            for seg in segment_set:
-                if seg in base_segment_map:
+        m = re.match("^.*evaluated: ([0-9a-f]*) ==> \\[([A-Z].*)?\\]", line)
+        if m:
+            cookie, segments = m.group(1), m.group(2)
+            if not segments:
+                base_cookie_map[cookie] = []
+            else:
+                for seg in segments.split(", "):
                     base_segment_map[seg].append(cookie)
-                else:
-                    cookie_jar.append(cookie)
-                    base_segment_map.update({seg: list(cookie_jar)})
-                    cookie_jar.clear()
+                    base_cookie_map[cookie].append(seg)
 
-templist = set()
-seg_list = list()
-cookie_list = list()
-maybe_dict = dict()
-segment_set = set()
-# segment_set = set(list(eval_segment_map.keys()) + list(base_segment_map.keys()))
+eval_cookie_map = collections.defaultdict(list)
+eval_segment_map = collections.defaultdict(list)
 
-for s in eval_segment_map.keys():
-    segment_set.add(s)
+with open("evaluator-integration.log", "r") as f:
+    for line in f:
+        m = re.match("^.*evaluated: ([0-9a-f]*) ==> \\[([A-Z].*)?\\]", line)
+        if m:
+            cookie, segments = m.group(1), m.group(2)
+            if not segments:
+                eval_cookie_map[cookie] = []
+            else:
+                for seg in segments.split(", "):
+                    eval_segment_map[seg].append(cookie)
+                    eval_cookie_map[cookie].append(seg)
 
-for s in base_segment_map.keys():
-    segment_set.add(s)
+# print(len(base_cookie_map.keys()))
+# print(len(eval_cookie_map.keys()))
+# print(len(base_segment_map.keys()))
+# print(len(eval_segment_map.keys()))
 
 seg_list_eval = eval_segment_map.keys()
 seg_list_base = base_segment_map.keys()
-print(len(segment_set))
-print(len(eval_segment_map.keys()))
-print(len(base_segment_map.keys()))
+seg_set = set(list(seg_list_base) + list(seg_list_eval))
+seg_list = list()
+report_dict_plus = dict()
+report_dict_minus = dict()
 
 for seg in eval_segment_map.keys():
     templist = set(eval_segment_map[seg]).difference(set(base_segment_map[seg]))
-    # seg_list.append(seg)
+        # seg_list.append(seg)
     if len(templist) > 0:
         seg_list.append(seg)
         cookie_jar = list(templist)
-        maybe_dict.update({seg: cookie_jar})
+        report_dict_plus.update({seg: cookie_jar})
 
 seg_list.sort()
-delta = len(maybe_dict)
+delta = len(report_dict_plus)
 count = len(base_segment_map.keys())
 print('Segments with added cookies: ', delta, '/', count)
-# # print(maybe_dict)
+counter = 0
+for s in seg_list:
+    print('%d   %s  %d  %s' % (counter, s, len(report_dict_plus[s]), report_dict_plus[s]))
+    counter += 1
+print()
+
 seg_list.clear()
-maybe_dict.clear()
-cookie_jar.clear()
 
 for seg in base_segment_map.keys():
-    templist = set(base_segment_map[seg]).difference(set(eval_segment_map[seg]))
-    # seg_list.append(seg)
-    if len(templist) > 0:
+    if seg in eval_segment_map.keys():
+        templist = set(base_segment_map[seg]).difference(set(eval_segment_map[seg]))
+        if len(templist) > 0:
+            seg_list.append(seg)
+            cookie_jar = list(templist)
+            report_dict_minus.update({seg: cookie_jar})
+    else:
+        templist = set(eval_segment_map[seg])
         seg_list.append(seg)
         cookie_jar = list(templist)
-        maybe_dict.update({seg: cookie_jar})
-
-# for seg in base_segment_map.keys():
-#     if seg in seg_list_eval:
-#         templist = set(base_segment_map[seg].difference(set(eval_segment_map[seg]))
-#         seg_list.append(seg)
-#         if len(templist) > 0:
-#             cookie_jar = list(templist)
-#             maybe_dict.update({seg: cookie_jar})
-#     else:
-#         # templist = set(eval_segment_map.get(seg))
-#         seg_list.append(seg)
-#         cookie_jar = eval_segment_map[seg]
-#         maybe_dict.update({seg: cookie_jar})
-
+        report_dict_minus.update({seg: cookie_jar})
 
 seg_list.sort()
-delta = len(seg_list)
-cookie_list.clear()
+delta = len(report_dict_minus)
 count = len(base_segment_map.keys())
-# count = len(maybe_dict.values())
 print('Segments with missing cookies: ', delta, '/', count)
-
-
-# for seg in eval_segment_map.keys():
-#     templist  = set(base_segment_map.get(seg)).difference(set(eval_segment_map.get(seg)))
-#     # print(templist(type))
-#     # print(type(templist))
-#     seg_list.append(seg)
-#     if len(templist) > 0:
-#         cookie_list = list(templist)
-#         maybe_dict.update({seg: cookie_list})
-
-
-
-
-
-# for seg in eval_segment_map.keys():
-#     templist = set(eval_segment_map[seg]).difference(set(base_segment_map[seg]))
-#     # print(templist)
-#     seg_list.append(seg)
-#     if len(templist) > 0:
-#         cookie_list = list(templist)
-#         maybe_dict.update({seg: cookie_list})
-
-# seg_list.sort()
-# delta = len(maybe_dict)
-# # count = sum(len(v) for v in maybe_dict.values())
-# count = len(base_segment_map.items())
-# print('Segments with added cookies: ', delta, '/', count)
-
-# # print(len(maybe_dict.items()))
-# # count = 0
-# # for j in maybe_dict.items():
-# #     count += len(maybe_dict[j])
-# # print('motherfuckin: ', count)
-
-# # print(count)
-# # print(maybe_dict)
-# maybe_dict.clear()
-# seg_list.clear()
-# # # print(maybe_dict)
-
-# for seg in eval_segment_map.keys():
-#     templist  = set(base_segment_map.get(seg)).difference(set(eval_segment_map.get(seg)))
-#     # print(templist(type))
-#     # print(type(templist))
-#     seg_list.append(seg)
-#     if len(templist) > 0:
-#         cookie_list = list(templist)
-#         maybe_dict.update({seg: cookie_list})
-
-
-# seg_list.sort()
-# delta = len(maybe_dict)
-# cookie_list.clear()
-# print('Segments with added cookies: ', delta, '/', count)
-# # print(maybe_dict)
-
-# for c in eval_cookie_map.key():
-#     templist = set(eval_cookie_map[c]).difference(set(base_cookie_map[c]))
-#     cookie_list.append(c)
-#     if len(templist) > 0:
-#         seg_list = list(templist)
-#         maybe_dict.update({c: seg_list})
+counter = 0
+for s in seg_list:
+    print('%d   %s  %d  %s' % (counter, s, len(report_dict_minus[s]), report_dict_minus[s]))
+    counter += 1
+print()
 
 end = time.time()
 print(end - start)
